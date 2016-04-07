2235ff6bf8f48eeb5d16bc45717e6882e3e0cf9e
Yakov <lostcorsair@yahoo.com>
2016-04-06 22:41:27 -0700

    Update ex4_21.cpp

--- ch04/ex4_21.cpp
+++ ch04/ex4_21.cpp
@@ -1,3 +1,5 @@
+// in this case you can change vector as desired( I think that author wants as to change the elements of this 
+// vector) and print it at the same time
 #include <iostream>
 #include <vector>
 
@@ -9,8 +11,8 @@ int main()
 {
     vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    for (auto i : ivec) {
-        cout << ((i & 0x1) ? i * 2 : i) << " ";
+    for (auto &i : ivec) {
+        cout << ((i & 0x1) ? (i *= 2) : i) << " ";
     }
     cout << endl;
     
